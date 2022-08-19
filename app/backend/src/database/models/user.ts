df4743afe66afc43e823273ce558f26512b9d0ef
raphaelfiorda <raphaelfiorda@hotmail.com>
2022-08-18 21:24:43 -0300

    chore: (req 01) Configura migration e model da tabela Users

---
+++ app/backend/src/database/models/user.ts
@@ -0,0 +1,39 @@
+import { INTEGER, STRING, Model } from 'sequelize';
+import db from '.';
+
+class User extends Model {
+  id!: number;
+  username!: string;
+  role!: string;
+  email!: string;
+  password!: string;
+}
+
+User.init({
+  id: {
+    type: INTEGER,
+    primaryKey: true,
+    autoIncrement: true,
+  },
+  username: {
+    type: STRING,
+    allowNull: false,
+  },
+  role: {
+    type: STRING,
+    allowNull: false,
+  },
+  email: {
+    type: STRING,
+    allowNull: false,
+  },
+  password: {
+    type: STRING,
+    allowNull: false,
+  },
+}, {
+  sequelize: db,
+  modelName: 'users',
+});
+
+export default User;
