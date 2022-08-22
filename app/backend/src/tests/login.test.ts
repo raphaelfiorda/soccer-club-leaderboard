17c97844ff0fc407655edbc914597e0b0f8989ab
raphaelfiorda <raphaelfiorda@hotmail.com>
2022-08-22 19:58:01 -0300

    test: Implementa testes para caso de falha em POST /login

--- app/backend/src/tests/login.test.ts
+++ app/backend/src/tests/login.test.ts
@@ -14,7 +14,7 @@ chai.use(chaiHttp);
 const { expect } = chai;
 
 describe('Testa a integração da rota de /login', () => {
-  describe('Verifica o método login em caso de sucesso', async () => {
+  describe('Verifica o caso de sucesso', async () => {
     beforeEach(() => {
       Sinon.stub(User, 'findOne').resolves(userLoginMock as User);
       Sinon.stub(bcryptjs, 'compare').resolves(true);
@@ -39,6 +39,41 @@ describe('Testa a integração da rota de /login', () => {
         .send(loginMock);
 
       expect(response.body.token).to.equal('token');
+    });
+  });
+
+  describe('Verifica caso de falha na validação da requisição', async() => {
+    it('Retorna o status e mensagem de erro', async () => {
+      const errorResponse = await chai.request(app)
+        .post('/login')
+        .send('');
+
+      const { status, body: { message } } = errorResponse;
+
+      expect(status).to.equal(400);
+      expect(message).to.equal('All fields must be filled');
+    });
+  });
+
+  describe('Verifica caso o usuário não seja cadastrado ou os dados são inválidos', async() => {
+    beforeEach(() => {
+      Sinon.stub(User, 'findOne').resolves(null);
+      Sinon.stub(bcryptjs, 'compare').resolves(false);
+    });
+
+    afterEach(() => {
+      Sinon.restore();
+    })
+
+    it('Retorna o status e mensagem de erro', async () => {
+      const errorResponse = await chai.request(app)
+        .post('/login')
+        .send(loginMock);
+
+      const { status, body: { message } } = errorResponse;
+
+      expect(status).to.equal(401);
+      expect(message).to.equal('Incorrect email or password');
     });
   });
 });
